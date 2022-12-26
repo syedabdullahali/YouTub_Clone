9c27030379beeb226efa8ade94f58e2d56d33263
syedabdullahali <syedabdullah380@gmail.com>
2022-12-26 16:53:00 +0530

    added channel_card_feat✨

--- youtub_app/src/components/ChannelCard.jsx
+++ youtub_app/src/components/ChannelCard.jsx
@@ -1,11 +1,42 @@
-import React from 'react'
+import {Box,CardContent,CardMedia,Typography} from '@mui/material'
+import { CheckCircle } from '@mui/icons-material';
+import {Link} from 'react-router-dom'
 
-const ChannelCard = () => {
-  return (
-    <div> 
-      hello
-    </div>
+import { demoProfilePicture } from '../utils/constants';
+
+const ChannelCard = ({channeLdetail}) => (
+    <Box sx={{
+      boxShadow:'none',
+      borderRadius:'20px',
+
+
+    }}> 
+
+    <Link to ={`/channel/${channeLdetail?.id?.channelId}`}>
+    <CardContent sx={
+      {display:'flex'
+      ,justifyContent:'center',
+      flexDirection:'column',
+      textAlign:'center',
+      color:'#fff'
+      }}>
+
+      <CardMedia
+      image={channeLdetail?.snippet?.thumbnails?.high.url|| demoProfilePicture}
+      alt={channeLdetail?.snippet?.title}
+      sx={{borderRadius:'50%',height:'180px',width:'180px',mb:2,border:'1px solid #e3e3e3'}}
+      />
+
+      <Typography variant='h6'>
+       {channeLdetail?.snippet?.title}
+       <CheckCircle sx={{ fontSize: 15, color: 'gray', ml: '5px' }} />
+
+      </Typography>
+
+    </CardContent>
+    </Link>
+
+    </Box>
   )
-}
 
 export default ChannelCard
